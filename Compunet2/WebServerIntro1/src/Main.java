7af0d3f50fada0417d33d9c562f8059cbfdac9ba
miguelperezdev <perezojedamiguel99@gmail.com>
2026-02-05 14:36:15 -0500

    Version 2

--- Compunet2/WebServerIntro1/src/Main.java
+++ Compunet2/WebServerIntro1/src/Main.java
@@ -2,52 +2,87 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Servidor HTTP básico en Java
+ * Escucha en el puerto 5000 y responde con un HTML simple
+ */
 public class Main {
 
-    public static void main(String[] args) throws Exception{
-      new Main();
+    public static void main(String[] args) {
+        try {
+            new Main();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Main() throws IOException{
+    /**
+     * Constructor del servidor
+     * Crea el ServerSocket y queda escuchando conexiones
+     */
+    public Main() throws IOException {
         ServerSocket serverSocket = new ServerSocket(5000);
-        System.out.println("Waiting for connection ..."); //Esperando la connecion del cliente al servidor
+        System.out.println("Esperando conexiones...");
 
-     while (true) { //Este while nos permite tener multiples ventanas activas
-         // de otro modo no nos deja recargar ni crear mas de 1 conexion 
-         Socket socket = serverSocket.accept();
-         System.out.println("Connected to the server ");// Confirmando el servidor que ya el cliente entro
+        // Permite múltiples clientes conectados
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Cliente conectado");
 
-
-         // ESto nos permite tener un mejor flujo de datos
-
-         InputStream is = socket.getInputStream(); //Request
-         OutputStream os = socket.getOutputStream(); //Response
-
-         BufferedReader br = new BufferedReader(
-                 new InputStreamReader(is)
-         ); // Me permite ver lo de internet a nivel de texto
-
-         BufferedWriter bw = new BufferedWriter(
-                 new OutputStreamWriter(os)
-         );
-
-         bw.write("HTTP/1.1 200 OK\r\n");
-         bw.write("Content-Type: text/html\r\n");
-         bw.write("Content-Length: 34\r\n");
-         bw.write("\r\n");
-         bw.write("<html><body>HOLA MUNDO</body></html>");
-         bw.flush();
-
-         //Leo lo que me mando el cliente
-         String linea = "";
-         while ((linea = br.readLine()) != null && !linea.isEmpty()) {
-             System.out.println(linea);
-         }
-     }
-        //Esto es lo que permite que el servidor vea informacion nuestra
+            // Se crea un hilo por cada cliente
+            HttpProcess thread = new HttpProcess(socket);
+            thread.start();
+        }
     }
 
-    //http:/localhost:5000/
+    /**
+     * Clase que maneja cada conexión HTTP
+     */
+    public class HttpProcess extends Thread {
+
+        private Socket socket;
+
+        public HttpProcess(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Flujo de entrada (Request)
+                InputStream is = socket.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                // Flujo de salida (Response)
+                OutputStream os = socket.getOutputStream();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+
+                // Respuesta HTTP
+                String body = "<html><body>HOLA MUNDO</body></html>";
+
+                bw.write("HTTP/1.1 200 OK\r\n");
+                bw.write("Content-Type: text/html; charset=UTF-8\r\n");
+                bw.write("Content-Length: " + body.length() + "\r\n");
+                bw.write("\r\n"); // Línea en blanco obligatoria
+                bw.write(body);
+                bw.flush();
+
+                // Leer lo que envió el cliente (request HTTP)
+                String linea;
+                while ((linea = br.readLine()) != null && !linea.isEmpty()) {
+                    System.out.println(linea);
+                }
+
+                // Cerrar recursos
+                br.close();
+                bw.close();
+                socket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            }
+        }
+    }
 }
-
-
