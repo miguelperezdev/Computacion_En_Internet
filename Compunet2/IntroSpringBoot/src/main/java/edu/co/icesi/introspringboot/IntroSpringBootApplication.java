14161354c71f62f459470330df7433d0fb68bce3
Miguel <perezojedamiguel99@gmail.com>
2026-02-26 14:33:56 -0500

    S4

---
+++ Compunet2/IntroSpringBoot/src/main/java/edu/co/icesi/introspringboot/IntroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.co.icesi.introspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroSpringBootApplication.class, args);
+    }
+
+}
